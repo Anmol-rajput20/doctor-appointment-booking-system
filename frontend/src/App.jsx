4b30ee0257a55d1e14bc7e3e727a6a22965dfb6a
Anmol <anmolrajput1601@gmail.com>
2026-06-22 19:27:02 +0530

    Initial frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Routes , Route } from 'react-router-dom';
+import Home from './pages/home';
+import Doctor from './pages/Doctor';
+import Login from './pages/Login';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import MyAppoint from './pages/MyAppoint';
+import MyProfile from './pages/MyProfile';
+import Appointment from './pages/Appointment';
+
+const App = () => {
+  return (
+    <div className="mx-4 sm:mx-[10%]">
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/doctor' element={<Doctor/>} />
+        <Route path='/doctor/:speciality' element={<Doctor/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/appointment' element={<MyAppoint/>} />
+        <Route path='/my-profile' element={<MyProfile/>} />
+        <Route path='/appointment/:docId' element={<Appointment/>} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
