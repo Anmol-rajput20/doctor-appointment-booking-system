a0bf257bd4477eb9207d478acd14323c748d7019
Anmol <anmolrajput1601@gmail.com>
2026-07-01 13:02:01 +0530

    Implement responsive contact page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -26,17 +26,17 @@ const About = () => {
 
       <div className='flex flex-col md:flex-row mb-20'>
 
-        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-black hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
           <b>EFFICIENCY</b>
           <p>Streamlined Appointment Scheduling That Fits Into Your Busy Lifestyle.</p>
         </div>
 
-        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-black hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
           <b>CONVENIENCE</b>
           <p>Access To A Network Of Trusted Healthcare Professionals In Your Area.</p>
         </div>
 
-        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-black hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
           <b>PERSONALIZATION</b>
           <p>Tailored Recommendations And Reminders To Help You Stay On Top Of Your Health.</p>
         </div>
