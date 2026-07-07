5452c8b0c712edee91fbb9f9e8c602d3ca37ec28
Anmol <anmolrajput1601@gmail.com>
2026-07-07 15:37:02 +0530

    Implement admin and doctor login interface

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Login from './pages/Login'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Login/>
+    </div>
   )
 }
-
+ 
 export default App
