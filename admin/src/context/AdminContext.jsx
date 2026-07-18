a8c2350c158768a0c9cae0de7fbf1f5e0aa2e51d
Anmol <anmolrajput1601@gmail.com>
2026-07-18 20:04:30 +0530

    Implement doctor list and availability management

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -1,4 +1,6 @@
+import axios from "axios";
 import { createContext, useState } from "react";
+import { toast } from "react-toastify";
 
 
 export const AdminContext = createContext()
@@ -6,12 +8,43 @@ export const AdminContext = createContext()
 const AdminContextProvider = (props) => {
 
     const [aToken,setAToken] = useState(localStorage.getItem('aToken') ?localStorage.getItem('aToken') : "")
-
+    const [doctors,setDoctors] = useState([])
     const backendurl = import.meta.env.VITE_BACKEND_URL
+
+    const getAllDoctors = async () => {
+        console.log("get all doctors called")
+        try {
+            const {data} = await axios.post(backendurl + '/api/admin/all-doctors',{} , {headers : {aToken,},})
+            console.log(data);
+            if(data.success){
+                setDoctors(data.doctors)
+                console.log(data.doctors)
+            }else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+    const changeAvailability = async(docId) => {
+        try {
+            const {data} = await axios.post(backendurl + '/api/admin/change-availability',{docId},{headers:{aToken}})
+            if(data.success) {
+                toast.success(data.message)
+                getAllDoctors()
+            } else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
 
     const value = {
         aToken,setAToken,
-        backendurl
+        backendurl,doctors,
+        getAllDoctors,changeAvailability
     }
 
     return (
