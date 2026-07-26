5ca8f98e2d107490256ceebed02586a4000aeb83
Anmol <anmolrajput1601@gmail.com>
2026-07-26 19:43:02 +0530

    Add admin dashboard with doctors, patients and appointment overview

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -10,6 +10,7 @@ const AdminContextProvider = (props) => {
     const [aToken,setAToken] = useState(localStorage.getItem('aToken') ?localStorage.getItem('aToken') : "")
     const [doctors,setDoctors] = useState([])
     const [appointments,setAppointments] = useState([])
+    const [dashData,setDashData] = useState(false)
     const backendurl = import.meta.env.VITE_BACKEND_URL
 
     const getAllDoctors = async () => {
@@ -72,12 +73,26 @@ const AdminContextProvider = (props) => {
         }
     }
 
+    const getDashData = async () => {
+        try {
+            const {data} = await axios.get(backendurl+'/api/admin/dashboard',{headers:{aToken}})
+            if(data.success){
+                setDashData(data.dashData)
+                console.log(data.dashData)
+            }else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
     const value = {
         aToken,setAToken,
         backendurl,doctors,
         getAllDoctors,changeAvailability,
         appointments,setAppointments,getAllAppointments,
-        cancelAppointmentAdmin
+        cancelAppointmentAdmin,dashData,getDashData
     }
 
     return (
